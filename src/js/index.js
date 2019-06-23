0d43d89064cfeb2556ff4e2124008892ac713d1f
Abuzarali <abuzarali0572@gmail.com>
2019-06-23 14:42:03 +0530

    First Commit & Basic Skeleton Project - Refer Github For Instructions to implement it

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+//jshint esversion:6
+
+import x from './test';
+alert('Hola Amigo!');
+
+x();
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+//jshint esversion:6
+export default function() { console.log(`The Setup Is Complete, Successfully Imported Files Using ES6 And It Is Backward Compactible`);
+};
